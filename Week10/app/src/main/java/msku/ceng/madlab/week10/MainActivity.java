0df9ceea44ab20211822990579f3a869a835d616
ardattoprak <ardattoprak@gmail.com>
2023-12-17 11:25:59 +0300

    week10

---
+++ Week10/app/src/main/java/msku/ceng/madlab/week10/MainActivity.java
@@ -0,0 +1,202 @@
+package msku.ceng.madlab.week10;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class MainActivity extends AppCompatActivity {
+    EditText txtURL;
+    Button btnDownload;
+    ImageView imgView;
+    //Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtURL = (EditText) findViewById(R.id.txtURL);
+        btnDownload = (Button) findViewById(R.id.btnDownload);
+        imgView = (ImageView) findViewById(R.id.imgView);
+        btnDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int permission = ActivityCompat.checkSelfPermission(
+                        MainActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                if (permission != PackageManager.PERMISSION_GRANTED) {
+                    // We don't have permission so prompt the user
+                    ActivityCompat.requestPermissions(
+                            MainActivity.this,
+                            PERMISSIONS_STORAGE,
+                            REQUEST_EXTERNAL_STORAGE
+                    );
+                }
+                String fileName = "temp.jpg";
+                String imagePath = (Environment.getExternalStoragePublicDirectory
+                        (Environment.DIRECTORY_DOWNLOADS)).toString()
+                        + "/" + fileName;
+                downloadFile(txtURL.getText().toString(),imagePath);
+                preview(imagePath);
+            }
+        });
+    }
+
+    btnDownload.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int permission = ActivityCompat.checkSelfPermission(
+                    MainActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            if (permission != PackageManager.PERMISSION_GRANTED) {// We don't have permission so prompt the user
+                ActivityCompat.requestPermissions(
+                        MainActivity.this,
+                        PERMISSIONS_STORAGE,
+                        REQUEST_EXTERNAL_STORAGE
+                );
+            }
+            DownloadTask task = new DownloadTask();
+            String[] urls = new String[1];
+            urls[0] = txtURL.getText().toString();
+            task.execute(urls);
+        }
+    };
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_EXTERNAL_STORAGE) {
+            if (grantResults.length == 2 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                String fileName = "temp.jpg";
+                String imagePath = (Environment.getExternalStoragePublicDirectory
+                        (Environment.DIRECTORY_DOWNLOADS)).toString()
+                        + "/" + fileName;
+                downloadFile(txtURL.getText().toString(), imagePath);
+                preview(imagePath);
+            } else {
+                Toast.makeText(this, "External Storage permission not granted",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    void downloadFile(String strURL, String imagePath){
+        try {
+            URL url = new URL(strURL);
+            URLConnection connection = url.openConnection();
+            connection.connect();
+            InputStream input = new BufferedInputStream(url.openStream(), 8192);
+            OutputStream output = new FileOutputStream(imagePath);
+            byte data[] = new byte[1024];
+            int count;
+            while ((count = input.read(data)) != -1) {
+                output.write(data, 0, count);
+            }
+            output.flush();
+            output.close();
+            input.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void preview(String imagePath ){
+        Bitmap image = BitmapFactory.decodeFile(imagePath);
+        float w = image.getWidth();
+        float h = image.getHeight();
+        int W = 400;
+        int H = (int) ( (h*W)/w);
+        Bitmap b = Bitmap.createScaledBitmap(image, W, H, false);
+        imgView.setImageBitmap(b);
+    }
+
+    class DownloadTask extends AsyncTask<String, Integer, Bitmap> {
+
+
+        ProgressDialog PD;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            PD = new ProgressDialog(MainActivity.this);
+            PD.setMax(100);
+            PD.setIndeterminate(false);
+            PD.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            PD.setTitle("Downloading");
+            PD.setMessage("Please wait..");
+            PD.show();
+        }
+
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            String fileName = "temp.jpg";
+            File path = (Environment.getExternalStoragePublicDirectory
+                    (Environment.DIRECTORY_DOWNLOADS));
+            path.mkdirs();
+
+            String imagePath = (Environment.getExternalStoragePublicDirectory
+                    (Environment.DIRECTORY_DOWNLOADS)).toString()
+                    + "/" + fileName;
+            downloadFile(urls[0],imagePath);
+            return scaleBitmap(imagePath);
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+            PD.setProgress(progress[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            imgView.setImageBitmap(bitmap);
+        }
+
+
+
+    }
+
+
+    private Bitmap scaleBitmap(String imagePath){
+        Bitmap image = BitmapFactory.decodeFile(imagePath);
+        float w = image.getWidth();
+        float h = image.getHeight();
+        int W = 400;
+        int H = (int) ( (h*W)/w);
+        Bitmap bitmap = Bitmap.createScaledBitmap(image, W, H, false);
+        return bitmap;
+    }
+
+
+}
+
